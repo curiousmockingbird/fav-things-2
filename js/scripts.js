703b200f7a09c731011a4f349f5a60c79c5415fd
Harold Mesa <harold.mesa@icloud.com>
2022-08-15 16:42:49 -0500

    Favorite things arrays

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", function (){
+const form = document.getElementById("form");
+
+function gatherInput (event) {
+    event.preventDefault();
+
+    const input1 = document.getElementById("firstThing").value;
+    const input2 = document.getElementById("secondThing").value;
+    const input3 = document.getElementById("thirdThing").value;
+
+    const toArray = [input1, input2, input3];
+    
+    const newArray = toArray.concat('Reading','Jogging');
+
+    const ul = document.createElement("ul");
+    form.after(results);
+
+    const li1 = document.createElement('li');
+    li1.innerHTML = newArray[0];
+    ul.append(li1);
+
+    const li2 = document.createElement('li');
+    li2.innerHTML = newArray[1];
+    ul.append(li2);
+    
+    const li3 = document.createElement("li");
+    li3.innerHTML = newArray[2];
+    ul.append(li3);
+
+};
+
+
+
+
+function clearForm () {
+    document.getElementById("firstThing").value = null;
+    document.getElementById("secondThing").value = null;
+    document.getElementById("thirdThing").value = null;
+};
+
+
+form.addEventListener("submit", gatherInput);
+form.addEventListener("submit", clearForm);
+
+});
