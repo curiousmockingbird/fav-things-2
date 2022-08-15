065dc03b55eafec5c41b9cad770686e89e0d6f2c
Harold Mesa <harold.mesa@icloud.com>
2022-08-15 18:57:02 -0500

    Final commit

--- js/scripts.js
+++ js/scripts.js
@@ -13,23 +13,29 @@ function gatherInput (event) {
     const newArray = toArray.concat('Reading','Jogging');
 
     const ul = document.createElement("ul");
-    form.after(results);
+    form.after(ul);
 
+    
     const li1 = document.createElement('li');
     li1.innerHTML = newArray[0];
     ul.append(li1);
-
+    
     const li2 = document.createElement('li');
     li2.innerHTML = newArray[1];
     ul.append(li2);
     
     const li3 = document.createElement("li");
     li3.innerHTML = newArray[2];
-    ul.append(li3);
+    ul.append(li3); 
 };
 
 
 
+/* newArray.forEach(function(item) {
+   let li = document.createElement("li");
+   li.innerHTML = item;
+   ul.append(li);
+}); */
 
 function clearForm () {
     document.getElementById("firstThing").value = null;
